22ee041b13f0d453f6d65cba4d8fc598b8d05cf2
LorenzoDev <lorenzoegidioms@gmail.com>
2026-05-31 18:18:48 -0300

    Update idt.c

--- OS/kernel/idt.c
+++ OS/kernel/idt.c
@@ -1,3 +1,31 @@
+/*
+* Management of the Interrupt Descriptor Table (IDT).
+*
+* The IDT is a structure used by the CPU to locate
+* the handlers of interrupts and exceptions.
+*
+* This module allows configuring table entries,
+* associating interrupt vectors with kernel functions.
+*
+* Each entry contains the handler's address, the code
+* segment selector, and the access attributes required
+* by the processor.
+*/
+
+/*
+* Gerenciamento da Interrupt Descriptor Table (IDT).
+*
+* A IDT é uma estrutura utilizada pela CPU para localizar
+* os tratadores (handlers) de interrupções e exceções.
+*
+* Este módulo permite configurar entradas da tabela,
+* associando vetores de interrupção a funções do kernel.
+*
+* Cada entrada contém o endereço do handler, o seletor
+* de segmento de código e os atributos de acesso exigidos
+* pelo processador.
+*/
+
 #include "../include/types.h"
 #include "idt.h"
 

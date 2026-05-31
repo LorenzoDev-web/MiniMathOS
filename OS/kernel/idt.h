2ed9f385e156fe2cdc01873608160dd50196492e
LorenzoDev <lorenzoegidioms@gmail.com>
2026-05-31 15:14:10 -0300

    Create idt.h

---
+++ OS/kernel/idt.h
@@ -0,0 +1,25 @@
+#ifndef IDT_H
+#define IDT_H
+
+#include "../include/types.h"
+
+typedef struct {
+
+    u16 offset_low;
+    u16 selector;
+    u8 zero;
+    u8 type_attr;
+    u16 offset_high;
+
+} __attribute__((packed)) IDTEntry;
+
+typedef struct {
+
+    u16 limit;
+    u32 base;
+
+} __attribute__((packed)) IDTPointer;
+
+void idt_init();
+
+#endif

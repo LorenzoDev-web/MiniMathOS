fa396a898902507fc9d1779f955c0f8491d4a99a
LorenzoDev <lorenzoegidioms@gmail.com>
2026-05-31 15:17:12 -0300

    Create string.c
    Implement string manipulation functions: strcmp, strcpy, and starts_with.

---
+++ OS/lib/string.c
@@ -0,0 +1,39 @@
+/* ========================= */
+/* STRING */
+/* ========================= */
+
+int strcmp(char* a, char* b) {
+
+    while (*a && *b) {
+
+        if (*a != *b)
+            return 0;
+
+        a++;
+        b++;
+    }
+
+    return (*a == 0 && *b == 0);
+}
+
+void strcpy(char* dst, char* src) {
+
+    while (*src)
+        *dst++ = *src++;
+
+    *dst = 0;
+}
+
+int starts_with(char* str, char* prefix) {
+
+    while (*prefix) {
+
+        if (*str != *prefix)
+            return 0;
+
+        str++;
+        prefix++;
+    }
+
+    return 1;
+}

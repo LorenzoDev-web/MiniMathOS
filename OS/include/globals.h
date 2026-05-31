862cde1787707515318de1e19df9cf3239f173b5
LorenzoDev <lorenzoegidioms@gmail.com>
2026-05-31 15:12:05 -0300

    Create globals.h

---
+++ OS/include/globals.h
@@ -0,0 +1,26 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include "types.h"
+#include "../script/runtime.h"
+
+extern char program[4096];
+extern int prog_len;
+
+extern int stop_program;
+
+extern unsigned char color;
+
+extern int x;
+extern int y;
+
+extern int label_count;
+
+extern int loop_count;
+
+extern int editing;
+
+extern char input[256];
+extern int input_len;
+
+#endif

b72e68d60229f7fdd37bb97c1135541d3cb2a794
LorenzoDev <lorenzoegidioms@gmail.com>
2026-05-31 15:16:22 -0300

    Create math.h

---
+++ OS/lib/math.h
@@ -0,0 +1,20 @@
+#ifndef KMATH_H
+#define KMATH_H
+
+float sin(float x);
+float cos(float x);
+float tan(float x);
+
+float sqrt(float x);
+
+float powi(float base,int exp);
+
+float ln(float x);
+
+float log10(float x);
+
+float fabs_simple(float x);
+
+float atof_simple(char* s);
+
+#endif

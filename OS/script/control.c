969d34a6daecabf01f12b7c523b0e63a3f2c58c9
LorenzoDev <lorenzoegidioms@gmail.com>
2026-05-31 18:26:14 -0300

    Update control.c

--- OS/script/control.c
+++ OS/script/control.c
@@ -1,3 +1,21 @@
+/*
+* Monitoring of the Escape key.
+*
+* Checks the PS/2 keyboard controller and detects when
+* the Esc key is pressed. When this occurs, the global
+* variable stop_program is set, allowing the interruption
+* of running programs or loops.
+*/
+
+/*
+* Monitoramento da tecla Escape.
+*
+* Verifica o controlador de teclado PS/2 e detecta quando
+* a tecla Esc é pressionada. Quando isso ocorre, a variável
+* global stop_program é definida, permitindo interromper
+* a execução de programas ou loops em andamento.
+*/
+
 #include "../types.h"
 
 extern u8 inb(u16 port);

eb4dc2113dd216871fbb825e66fa7c63b4b711b9
LorenzoDev <lorenzoegidioms@gmail.com>
2026-06-13 16:08:06 -0300

    Change print function parameter to const char*
    Updated print function to accept const char* for string literals.

--- OS/drivers/vga.h
+++ OS/drivers/vga.h
@@ -4,6 +4,7 @@
 #define VGA_H
 
 #include "../include/types.h"
+#include "stdint.h"
 
 extern int x;
 extern int y;
@@ -11,10 +12,12 @@ extern u8 color;
 
 void clear();
 void putc(char c);
-void print(char* s);
+void print(const char* s);
 void plot(int px,int py,char c);
 void print_center(char* s);
 void print_int(int n);
+void print_hex(uint32_t value);
+void print_reg(const char* name, uint32_t value);
 void shell();
 
 #endif
